57eed645b448f5d4ef172b82871ddbd145e83264
Roman <runandback@gmail.com>
2021-02-25 09:40:59 +0300

    Initial setup

---
+++ scripts/server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config(); // if we are not in priduction mode - it will load all variables from .env file
+} 
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('../routes/index');
+
+app.set('view engine', 'ejs'); // kind of HTML engine
+app.set('views', './views'); // all different views folder
+app.set('layout', 'layouts/layout'); // layout that kepp header, footer and other repeatable elements
+app.use(expressLayouts);
+app.use(express.static('public')); // where our styles and images - public files
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => console.log('connected to mongoose'));
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
